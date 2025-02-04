d810b6f95f25cbdf937a5a02018a99a38af2b2bb
swaroop.anand <swaroop.anand@activate.sg>
2025-02-04 12:11:45 +0800

    [Swaroop] Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Outlet } from "react-router";
+import { ReactRouterAppProvider } from "@toolpad/core/react-router";
+import type { Navigation } from "@toolpad/core/AppProvider";
+
+const NAVIGATION: Navigation = [
+  {
+    kind: "header",
+    title: "Main items",
+  },
+  {
+    title: "",
+    icon: <DashboardIcon />,
+  },
+];
+
+const BRANDING = {
+  title: "Cloudbox",
+};
+
+export default function App() {
+  return (
+    <ReactRouterAppProvider navigation={NAVIGATION} branding={BRANDING}>
+      <Outlet />
+    </ReactRouterAppProvider>
+  );
+}
---
+++ theme.ts
@@ -0,0 +1,11 @@
+"use client";
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  cssVariables: {
+    colorSchemeSelector: "data-toolpad-color-scheme",
+  },
+  colorSchemes: { light: true, dark: true },
+});
+
+export default theme;
