9a9bb5988e2de2145518bd24dd6fa1b018f6ff96
swaroop.anand <swaroop.anand@activate.sg>
2025-02-04 22:35:23 +0800

    [Swaroop] Added JSON scheme

--- src/components/FormRenderer.tsx
+++ src/components/FormRenderer.tsx
@@ -1,48 +1,37 @@
-import React, { useState } from "react";
-import { Button, Box, Grid2 } from "@mui/material";
+import React, { FormEvent } from "react";
+import { Box, Grid2 } from "@mui/material";
 import originalSchema from "../../schema.json";
-import { JSONData, renderField } from "../utils/render";
+import Form from "@rjsf/mui";
+import validator from "@rjsf/validator-ajv8";
+import { IChangeEvent } from "@rjsf/core";
 
 const DynamicFormRenderer: React.FC = () => {
-  const [formData, setFormData] = useState<JSONData>(originalSchema);
-
-  const handleSubmit = () => {
-    // const blob = new Blob([JSON.stringify(formData, null, 2)], {
-    //   type: "application/json",
-    // });
-    // const url = URL.createObjectURL(blob);
-    // const a = document.createElement("a");
-    // a.href = url;
-    // a.download = "formData.json";
-    // document.body.appendChild(a);
-    // a.click();
-    // document.body.removeChild(a);
-    // URL.revokeObjectURL(url);
-  };
-  const renderBaseItem = (baseItem) => {
-    return renderField({ ...baseItem, setFormData });
+  const handleSubmit = (
+    data: IChangeEvent<any, any, any>,
+    event: FormEvent<any>
+  ) => {
+    const { formData } = data;
+    const blob = new Blob([JSON.stringify(formData, null, 2)], {
+      type: "application/json",
+    });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "formData.json";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
   };
 
   return (
     <Box sx={{ padding: "20px" }}>
-      <Grid2 direction={"column"}>
-        <Grid2>
-          <form>
-            {originalSchema.project_base.map((baseItem) =>
-              renderBaseItem(baseItem)
-            )}
-          </form>
-        </Grid2>
-        <Grid2>
-          <Button
-            variant="contained"
-            color="primary"
-            sx={{ marginTop: "20px" }}
-            onClick={handleSubmit}
-          >
-            Submit
-          </Button>
-        </Grid2>
+      <Grid2>
+        <Form
+          schema={originalSchema}
+          validator={validator}
+          onSubmit={handleSubmit}
+        />
       </Grid2>
     </Box>
   );
